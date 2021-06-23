f219aff448ea256956f175454d758f83ba681836
habibbibani <habibbibani79@gmail.com>
2021-06-23 18:53:18 +0100

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -13,7 +13,7 @@
     $body="FROM : $email \n Message : $message";
     $mailheader = "From: $email \r\n";
 
-    mail($receiving_email_address,$subject,$body,$mailheader) or die("Error!")
+    mail($receiving_email_address,$subject,$body,$mailheader) or die("Error!");
   }
 
   
